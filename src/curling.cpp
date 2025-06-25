66410c717334e33611b10195af102e7414df120a
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2025-06-24 22:20:03 -0500

    Update curling.cpp

--- src/curling.cpp
+++ src/curling.cpp
@@ -138,7 +138,12 @@ void Request::updateURL() {
 }
 
 void Request::trim(std::string &s){
-    
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch){
+        return !std::isspace(ch);
+    }));
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch){
+        return !std::isspace(ch);
+    }).base(), s.end());
 }
 
 } // namespace curling
