d38f76c16760dcca25d81a98c1b7ba2a25060d99
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2025-06-25 23:34:24 -0500

    Update main.cpp

--- example2/main.cpp
+++ example2/main.cpp
@@ -7,13 +7,14 @@ int main(){
 
     Request request;
 
-    request.setMethod(Request::Method::GET)
+    request.setMethod(Request::Method::MIME)
            .setURL("https://httpbin.org/post")
            .setUserAgent("Bond/James-Bond/007")
            .addFormField("formKey", "formValue")
            .addFormFile("file", "test.txt")
-           .setTimeout(10)
            .setConnectTimeout(5);
+           .setTimeout(10)
+           
 
     auto response = request.send();
 
