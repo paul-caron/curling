4508f70f8d0b8bdb01e23ebefd67df10fe1c5dea
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2025-06-27 23:04:48 -0500

    Update curling.cpp

--- src/curling.cpp
+++ src/curling.cpp
@@ -183,9 +183,9 @@ size_t Request::HeaderCallback(char* buffer, size_t size, size_t nitems, void* u
     if (colonPos != std::string::npos) {
         std::string key = headerLine.substr(0, colonPos);
         std::string value = headerLine.substr(colonPos + 1);
-        Request::trim(key);
-        Request::trim(value);
-        Request::toLowerCase(key);
+        trim(key);
+        trim(value);
+        toLowerCase(key);
         (*headerMap)[key].push_back(value);
     }
 
