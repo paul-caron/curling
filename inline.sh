27705192b01b1fa0f96bc67c10f68098cb3ccaa5
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2025-06-29 21:45:53 -0500

    Update inline.sh

--- inline.sh
+++ inline.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Script: inline.sh
+# Purpose: Modifies a C++ header file by inserting the `inline` keyword before each
+# member function definition of the `curling::Request` class.
+# Notes: This is useful when using header-only libraries to prevent multiple definition errors
+# during the linking phase when including the header in multiple translation units.
+
 sed -i '/Request::/ {
   /^[[:space:]]*inline/ ! {
     /(\|);$/ s/^[[:space:]]*/&inline /
