57a1b54eb63dbd7668cf9121b29f93aab4918efb
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2025-07-01 13:04:14 -0500

    Update curling.cpp

--- src/curling.cpp
+++ src/curling.cpp
@@ -85,7 +85,7 @@ Request& Request::setMethod(Method m) {
             break;
         case Method::HEAD:
             curl_easy_setopt(curlHandle.get(), CURLOPT_NOBODY, 1L);
-            curl_easy_setopt(curlHandle.get(), CURLOPT_HEADER, 1L);
+            //curl_easy_setopt(curlHandle.get(), CURLOPT_HEADER, 1L);
             break;
     }
     return *this;
